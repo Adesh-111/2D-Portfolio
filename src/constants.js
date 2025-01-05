65d933656c9e3a53b9604b0209a474d737bd05dd
Adesh-111 <adeshabinesh25@gmail.com>
2025-01-05 17:01:10 +0530

    Completion of 2D game portfolio

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,19 @@
 export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript and Full Stack development these days.
+    I've made a couple of projects in that language. I also like Backend and Java. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/adesh-111" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree.  I'm currently pursuing my 3rd year of Computer Science Engineering degree.  It has been a mix of theory and practical learning, providing me a strong foundation in problem-solving and software development. Other than college I do some <a href="https://adesh-dev.vercel.app/certificates" target="_blank">Certifications </a>` ,
+  "sofa-table": `That's my sofa. I like to relax here. And Think of my errors and read some good books.`,
+  tv: `That's my TV. I like watching movies on it.`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1sPsfL6jVGy4Kg8RXN94AGmZT2kwFkM1A/view" target="_blank">Check it out?</a>
+  Contact me at adeshabinesh25@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio: It's built using ReactJS, NodeJS and features like responsiveness and interactivity. 
+    Explore my other projects, like the Google Gemini AI app clone, Book Notes and more! 
+    Learn more about my journey <a href="https://adesh-dev.vercel.app" target="_blank">here</a>..`,
+  library: `These are my resources, a mix of inspiration, creativity, and knowledge. They keep me curious and push me to explore new horizons, even if some remain unfinished. Each one represents a step in my journey of growth and discovery.`,
+  exit: `If you want to exit Adesh's portfolio, just close the tab. 
+    But I hope you’ll stay connected and follow my journey in development!`,
+};
